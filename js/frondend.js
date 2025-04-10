0ad30f3def438dc2f5902bd7cd6d5ff360d3a95a
KhonThanh <tangkhonthanh@gmail.com>
2025-04-10 16:02:17 +0700

    update html + css + js
    update html + css + js

--- js/frondend.js
+++ js/frondend.js
@@ -1,3 +1,27 @@
+// js phần component
+
+document.addEventListener("DOMContentLoaded", () => {
+    const includeElements = document.querySelectorAll("[data-include]");
+
+    includeElements.forEach(async el => {
+        const file = el.getAttribute("data-include");
+        if (!file) return;
+
+        try {
+            const response = await fetch(file);
+            if (!response.ok) throw new Error("File not found");
+
+            const text = await response.text();
+            el.innerHTML = text;
+        } catch (err) {
+            el.innerHTML = `<p style="color: red;">Không tải được file: ${file}</p>`;
+            console.error(err);
+        }
+    });
+});
+
+
+// js banner chỗ trang chủ
 $(document).ready(function () {
     const $banner = $(".sidebar__banner");
 
@@ -23,6 +47,35 @@ $(document).ready(function () {
         });
     } else {
         console.log("Không tìm thấy .sidebar__banner – Slick không khởi tạo.");
+    }
+});
+
+// js cho banner trang con
+$(document).ready(function () {
+    const $banner = $(".big__banner");
+
+    if ($banner.length) {
+        const $images = $banner.children("div");
+        const currentCount = $images.length;
+
+        if (currentCount < 2) {
+            const clonesNeeded = 3 - currentCount;
+
+            for (let i = 0; i < clonesNeeded; i++) {
+                const $clone = $images.eq(0).clone();
+                $banner.append($clone);
+            }
+        }
+        $banner.slick({
+            infinite: true,
+            speed: 500,
+            dots: false,
+            autoplay: true,
+            arrows: false,
+            touchMove: false,
+        });
+    } else {
+        console.log("Không tìm thấy .big__banner – Slick không khởi tạo.");
     }
 });
 
@@ -57,3 +110,5 @@ $(document).ready(function () {
         console.log("Không tìm thấy .sidebar__banner – Slick không khởi tạo.");
     }
 });
+
+
